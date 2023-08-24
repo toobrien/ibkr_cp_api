b462b5cff6857d81bc5c9d2e5eab1388f8ca4337
Taylor O'Brien <toobrien@amazon.com>
2023-08-23 21:12:11 -0700

    .

--- test.py
+++ test.py
@@ -1,9 +1,23 @@
-from requests import get, post
+from json       import dumps
+from requests   import get, post
 
 
 # ...
 
+HOST_URI = "https://localhost:5000"
 
 if __name__ == "__main__":
 
-    pass
+    res = post(
+            url     = f"{HOST_URI}/v1/api/trsrv/secdef",
+            verify  = False,
+            json    = {
+                        "conids": [ 
+                            645111234, 
+                            645111279, 
+                            645111251 
+                        ]
+                    }
+        )
+    
+    print(dumps(res.json(), indent = 2))
