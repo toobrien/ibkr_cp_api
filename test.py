7888ebe748b5f5877d15cfabac6f3fd142c783c9
Taylor O'Brien <toobrien@amazon.com>
2023-08-24 00:16:53 -0700

    .

--- test.py
+++ test.py
@@ -1,23 +1,63 @@
-from json       import dumps
+from json       import dumps, loads
 from requests   import get, post
-
+from sys        import argv
+from websocket  import create_connection
+from ssl        import CERT_NONE
 
 # ...
 
-HOST_URI = "https://localhost:5000"
+
+REST_URI    = "https://localhost:5000/v1/api"
+WS_URI      = "wss://localhost:5000/v1/api/ws"
+
+
+def secdef(conid: int):
+
+    res = post(
+                url     = f"{REST_URI}/trsrv/secdef",
+                verify  = False,
+                json    = {
+                            "conids": [ conid ]
+                        }
+            )
+        
+    print(dumps(res.json(), indent = 2))
+
+
+def ws_quote(conid: int):
+
+    res = post(f"{REST_URI}/tickle", verify = False)
+
+    if res.status_code != 200:
+
+        print(res.text)
+
+        exit()
+
+    ws = create_connection(
+            url     = WS_URI,
+            sslopt  = { "cert_reqs": CERT_NONE }
+        )
+    
+    ws.send(f'smd+{conid}+{{ "fields": [ "31", "84", "85", "86", "88" ] }}')
+
+    while(True):
+
+        msg = loads(ws.recv().decode("utf-8"))
+
+        print(dumps(msg, indent = 2))
+
+
+TESTS = [
+            secdef,
+            ws_quote
+        ]
+
+
 
 if __name__ == "__main__":
 
-    res = post(
-            url     = f"{HOST_URI}/v1/api/trsrv/secdef",
-            verify  = False,
-            json    = {
-                        "conids": [ 
-                            645111234, 
-                            645111279, 
-                            645111251 
-                        ]
-                    }
-        )
-    
-    print(dumps(res.json(), indent = 2))
+    test = int(argv[1])
+    args = argv[2:] if len(argv) > 2 else []
+
+    TESTS[test](*args)
