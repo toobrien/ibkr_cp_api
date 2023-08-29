166c6042dddc5a6a013ce6716ffc1e1776b70608
Taylor O'Brien <toobrien@amazon.com>
2023-08-28 17:27:02 -0700

    outline

--- opt_client.js
+++ opt_client.js
@@ -12,7 +12,52 @@ class opt_client {
 
     }
 
-    
+
+    async get_conids_ind(ul_sym, exp, lo_str, hi_str, step, right) {
+
+
+
+    }
+
+
+    async get_conids_stock(ul_sym, exp, lo_str, hi_str, step, right) {
+
+
+
+    }
+
+
+    async get_conids_fut(ul_sym, ul_month, exp, lo_str, hi_str, step, right) {
+
+
+
+    }
+
+
+    async set_handlers(msg_handler, err_handler, opn_handler, cls_handler) {
+
+        this.base_client.set_ws_handlers(
+            msg_handler = handler,
+            err_handler = err_handler,
+            opn_handler = opn_handler,
+            cls_handler = cls_handler
+        );
+
+    }
+
+
+    async sub_l1(conids) {
+
+
+
+    }
+
+
+    async unsub_l1(conids) {
+
+
+
+    }
 
 
 }
